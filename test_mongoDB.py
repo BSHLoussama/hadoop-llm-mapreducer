06c531b9e8bc0d68672bb6c70b121d1a788a14db
BSHLOussama <oussamaboussahla2017@gmail.com>
2025-04-30 16:59:30 +0100

    clean the repo

--- test_mongoDB.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-cli = MongoClient("mongodb://localhost:27017")
-col = cli["anxiety"]["guidelines"]
-
-doc = col.find_one({}, {"_id": 0, "title": 1, "vector": 1})
-print(doc["title"])
-print("Vector length:", len(doc["vector"]))   # should be 384
-print("First 5 numbers:", doc["vector"][:5])
